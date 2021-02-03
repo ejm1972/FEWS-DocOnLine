2297065345506fa9396617e381dab165bf95cba3
Eduardo Marturano <emartura@boldt.com.ar>
2021-02-03 20:37:12 -0300

    bd interfaz punto venta parametro

--- BD/Insertar Interfaz, Punto Venta y Parámetro.sql
+++ BD/Insertar Interfaz, Punto Venta y Parámetro.sql
@@ -21,42 +21,16 @@ select @Modo_Intrfz = 'PROD' --o 'HOMO'
 begin tran
 
 if not exists(select ID_INTERFAZ from INTERFACES where ID_INTERFAZ=@Interfaz)
-	INSERT INTO [INTERFACES]
-           ([ID_INTERFAZ] 
-           ,[INTERFAZ]
-           ,[CLAVE]
-           ,[ACTIVADO]
-           ,[CANT_OPERACIONES]
-           ,[CANT_ACUM_OPERACIONES]
-           ,[ID_CANAL_ACCESO]
-           ,[FLG_CONTROL]
-           ,[CUIT_SUSCRIPCION])
-    VALUES
-           (@Interfaz
-           ,@Nombre
-           ,@Pass
-           ,'S'
-           ,0
-           ,0
-           ,3
-           ,'N'
-           ,@Cuit)
+	INSERT INTO [INTERFACES]([ID_INTERFAZ],[INTERFAZ],[CLAVE],[ACTIVADO],[CANT_OPERACIONES],[CANT_ACUM_OPERACIONES],[ID_CANAL_ACCESO],[FLG_CONTROL],[CUIT_SUSCRIPCION])
+	VALUES (@Interfaz,@Nombre,@Pass,'S',0,0,3,'N',@Cuit)
 
 select @Param=ID_PARAMETRO from PARAMETROS where PARAMETRO='MODO_INTRFZ_'+CONVERT(varchar(4), @Interfaz)
 if @Param is null
 begin
 	select @Param = isnull(MAX(ID_PARAMETRO),0)+1 from PARAMETROS
 	
-	INSERT INTO [PARAMETROS]
-           ([ID_PARAMETRO]
-           ,[PARAMETRO]
-           ,[DESCRIPCION]
-           ,[TXT_AYUDA])
-     VALUES
-           (@Param
-           ,'MODO_INTRFZ_'+CONVERT(varchar(4), @Interfaz)
-           ,'Modo para '+CONVERT(varchar(4), @Interfaz)+'-'+@Nombre
-           ,'Flag de Modo de Trabajo para Interfaz '+@Nombre+' [HOMO|PROD]')
+	INSERT INTO [PARAMETROS]([ID_PARAMETRO],[PARAMETRO],[DESCRIPCION],[TXT_AYUDA])
+	VALUES (@Param,'MODO_INTRFZ_'+CONVERT(varchar(4), @Interfaz),'Modo para '+CONVERT(varchar(4), @Interfaz)+'-'+@Nombre,'Flag de Modo de Trabajo para Interfaz '+@Nombre+' [HOMO|PROD]')
 end
 
 select @Param_valor = ID_PARAMETRO_VALOR from PARAMETROS_VALOR where ID_PARAMETRO=@Param
@@ -64,18 +38,8 @@ if @Param_valor is null
 begin
 	select @Param_valor = isnull(MAX(ID_PARAMETRO_VALOR),0)+1 from PARAMETROS_VALOR
 
-	INSERT INTO [PARAMETROS_VALOR]
-           ([ID_PARAMETRO_VALOR]
-           ,[ID_PARAMETRO]
-           ,[VALOR]
-           ,[F_VIGENCIA_DESDE]
-           ,[F_VIGENCIA_HASTA])
-     VALUES
-           (@Param_valor
-           ,@Param
-           ,@Modo_Intrfz
-           ,'2019-01-01 00:00:00.000'
-           ,'9999-09-09 00:00:00.000')
+	INSERT INTO [PARAMETROS_VALOR]([ID_PARAMETRO_VALOR],[ID_PARAMETRO],[VALOR],[F_VIGENCIA_DESDE],[F_VIGENCIA_HASTA])
+	VALUES(@Param_valor,@Param,@Modo_Intrfz,'2019-01-01 00:00:00.000','9999-09-09 00:00:00.000')
 end
 
 
@@ -84,18 +48,8 @@ if @IdPV is null
 begin
 	select @IdPV = isnull(MAX(ID_PUNTO_VENTA),0)+1 from PUNTO_VENTA
 
-	INSERT [PUNTO_VENTA]
-           ([ID_PUNTO_VENTA]
-           ,[ID_INTERFAZ]
-           ,[PUNTO_VENTA]
-           ,[ACTIVADO]
-           ,[FLG_CONTROL])
-     VALUES
-           (@IdPV
-           ,@Interfaz
-           ,@PuntoVenta
-           ,'S'
-           ,'N')
+	INSERT [PUNTO_VENTA]([ID_PUNTO_VENTA],[ID_INTERFAZ],[PUNTO_VENTA],[ACTIVADO],[FLG_CONTROL])
+	VALUES(@IdPV,@Interfaz,@PuntoVenta,'S','N')
 end
 
 commit tran
