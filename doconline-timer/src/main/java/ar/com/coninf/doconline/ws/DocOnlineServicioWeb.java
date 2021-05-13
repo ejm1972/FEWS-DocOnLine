cf305a42bdf2d8f5f07a4874c79527962ea4e09f
Eduardo Marturano <emartura@boldt.com.ar>
2021-05-13 20:31:41 -0300

    2021 05 13

--- doconline-timer/src/main/java/ar/com/coninf/doconline/ws/DocOnlineServicioWeb.java
+++
@@ -1,9 +0,0 @@
-package ar.com.coninf.doconline.ws;
-
-import ar.com.coninf.doconline.rest.model.response.Response;
-
-public interface DocOnlineServicioWeb {
-	
-	public Response scheduleTimerSql(String mensaje);
-
-}
